ce538520e7a8f7e19d9016422a62550f37b62495
Abdul Ghani <abdul.ghani@volteo.com>
2020-01-18 15:38:11 +0530

    working condition

---
+++ YoutubeChannelSubscriber/script.js
@@ -0,0 +1,21 @@
+const allChannelsBox = document.querySelectorAll("#channel");
+let totalUnsubscribedChannels = 0;
+let count = 0;
+let sleep = 0;
+//for counting totalUnsubscribedChannels
+allChannelsBox.forEach(function(channel, index){
+	if(channel.children[1].innerText === "SUBSCRIBE"){	
+		totalUnsubscribedChannels++;
+	}});
+//for clicking all unsubscribed Channels
+allChannelsBox.forEach(function(channel, index){
+	if(channel.children[1].innerText === "SUBSCRIBE"){		
+		setTimeout(function() {
+		count++;
+		console.log("Current Channel ->" +channel.children[0].children["title"].innerText);
+        channel.children[1].children[0].children[0].click();
+        console.log("Subscribed! 😎"+ count +"out of"+totalUnsubscribedChannels+"channels");
+    	}, sleep);
+    	sleep += 15000;
+	}
+});
