64c09a60c61a393cfa81f1f0f537a5cbac7e1d2f
Abdul Ghani <abdul.ghani@volteo.com>
2020-10-03 16:42:56 +0530

    increased timemout limit to handle API rate limiting

--- YoutubeChannelSubscriber/script.js
+++ YoutubeChannelSubscriber/script.js
@@ -3,19 +3,29 @@ let totalUnsubscribedChannels = 0;
 let count = 0;
 let sleep = 0;
 //for counting totalUnsubscribedChannels
-allChannelsBox.forEach(function(channel, index){
-	if(channel.children[1].innerText === "SUBSCRIBE"){	
-		totalUnsubscribedChannels++;
-	}});
+allChannelsBox.forEach(function (channel, index) {
+    if (channel.children[1].innerText === "SUBSCRIBE") {
+        totalUnsubscribedChannels++;
+    }
+});
 //for clicking all unsubscribed Channels
-allChannelsBox.forEach(function(channel, index){
-	if(channel.children[1].innerText === "SUBSCRIBE"){		
-		setTimeout(function() {
-		count++;
-		console.log("Current Channel ->" +channel.children[0].children["title"].innerText);
-        channel.children[1].children[0].children[0].click();
-        console.log("Subscribed! 😎"+ count +"out of"+totalUnsubscribedChannels+"channels");
-    	}, sleep);
-    	sleep += 15000;
-	}
+allChannelsBox.forEach(function (channel, index) {
+    if (channel.children[1].innerText === "SUBSCRIBE") {
+        setTimeout(function () {
+            count++;
+            console.log(
+                "Current Channel ->" +
+                    channel.children[0].children["title"].innerText
+            );
+            channel.children[1].children[0].children[0].click();
+            console.log(
+                "Subscribed! 😎" +
+                    count +
+                    "out of" +
+                    totalUnsubscribedChannels +
+                    "channels"
+            );
+        }, sleep);
+        sleep += 30000;
+    }
 });
